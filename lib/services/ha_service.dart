d21e9bf21e8cb6a3f958fd9df6492ef5d7a17c06
Krzysiek19921 <jurczakkrzysztof@wp.pl>
2026-06-26 20:48:07 +0200

    update hive dashboard

--- lib/services/ha_service.dart
+++ lib/services/ha_service.dart
@@ -134,16 +134,16 @@ class HaService {
         delta8hEntity: "sensor.ul5_8h_delta",
         delta24hEntity: "sensor.ul5_24h_delta",
 
-        tempEntity: "sensor.waga_ul_5_temperatura_ula_5",
-        humidityEntity: "sensor.waga_ul_5_wilgotnosc_ula_5",
+        tempEntity: "sensor.waga_ul_5_temperatura_ula_2",
+        humidityEntity: "sensor.waga_ul_5_wilgotnosc_ula_2",
         pressureEntity: "sensor.waga_ul_5_cisnienie_ula_5",
 
         weight: _get(data, "sensor.waga_ul_5_waga_ula"),
         delta8h: _get(data, "sensor.ul5_8h_delta"),
         delta24h: _get(data, "sensor.ul5_24h_delta"),
 
-        temp: _get(data, "sensor.waga_ul_5_temperatura_ula_5"),
-        humidity: _get(data, "sensor.waga_ul_5_wilgotnosc_ula_5"),
+        temp: _get(data, "sensor.waga_ul_5_temperatura_ula_2"),
+        humidity: _get(data, "sensor.waga_ul_5_wilgotnosc_ula_2"),
         pressure: _get(data, "sensor.waga_ul_5_cisnienie_ula_5"),
       ),
 
